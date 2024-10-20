52349031001b503d0bc2c997ed8369cc52141f14
Yurii Hehediush <yuriihehed@Yuriis-MBP.cable.rcn.com>
2024-10-20 18:18:53 -0500

    Final Version of Project2 tested and runs!

--- Project1/Algorithms/src/main/java/com/gsd/algorithms/datastructures/binarysearchtree/TreeTraversalOrder.java
+++
@@ -1,8 +0,0 @@
-package com.gsd.algorithms.datastructures.binarysearchtree;
-
-public enum TreeTraversalOrder {
-  PRE_ORDER,
-  IN_ORDER,
-  POST_ORDER,
-  LEVEL_ORDER
-}
--- Project1/Algorithms/src/main/java/com/gsd/algorithms/sorting/InplaceSort.java
+++
@@ -1,6 +0,0 @@
-package com.gsd.algorithms.sorting;
-
-// A shared interface amongst sorting algorithms which
-public interface InplaceSort {
-  public void sort(int[] values);
-}
